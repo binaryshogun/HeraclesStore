e526dc3fb7bc62a86fad604b6aabbeceafc99d3c
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-07 17:15:17 +0300

    DbContext for catalog added.

---
+++ source/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CatalogItem> CatalogItems { get; set; } = null!;
+        public DbSet<CatalogBrand> CatalogBrands { get; set; } = null!;
+        public DbSet<CatalogType> CatalogTypes { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
+            builder.ApplyConfiguration(new CatalogTypeEntityTypeConfiguration());
+            builder.ApplyConfiguration(new CatalogItemEntityTypeConfiguration());
+        }
+    }
+}
