22740be40c8db4a017b0eb763c8e4480d52dd8c5
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-07 17:13:40 +0300

    Catalog domain exception added.

---
+++ source/Services/Catalog/Catalog.Api/Exceptions/CatalogDomainException.cs
@@ -0,0 +1,20 @@
+namespace Catalog.Api.Exceptions;
+
+public class CatalogDomainException : Exception
+{
+    public CatalogDomainException()
+    {
+
+    }
+
+    public CatalogDomainException(string message) : base(message)
+    {
+
+    }
+
+    public CatalogDomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+
+    }
+}
