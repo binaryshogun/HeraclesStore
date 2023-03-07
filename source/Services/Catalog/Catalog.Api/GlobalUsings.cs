612d4ccf52c8f2943d379c031171337093565dbe
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-07 17:15:03 +0300

    Global usings updated.

--- source/Services/Catalog/Catalog.Api/GlobalUsings.cs
+++ source/Services/Catalog/Catalog.Api/GlobalUsings.cs
@@ -1,1 +1,6 @@
 global using Catalog.Api.Exceptions;
+global using Catalog.Api.Models;
+global using Catalog.Api.Data.EntityConfigurations;
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
