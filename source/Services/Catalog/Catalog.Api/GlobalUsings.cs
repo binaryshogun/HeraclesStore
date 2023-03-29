a3ba3ac80b235342ce2e8d17c680b1b5b7f6baf8
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-29 20:46:45 +0300

    Global usings file updated.

--- source/Services/Catalog/Catalog.Api/GlobalUsings.cs
+++ source/Services/Catalog/Catalog.Api/GlobalUsings.cs
@@ -1,13 +1,24 @@
+global using Polly;
+global using Polly.Retry;
+
+global using Serilog;
+
+global using System.IO;
+
+global using Catalog.Api;
+global using Catalog.Api.Filters;
 global using Catalog.Api.Exceptions;
 global using Catalog.Api.Models;
 global using Catalog.Api.Data;
 global using Catalog.Api.Data.EntityConfigurations;
 
+global using Microsoft.Data.SqlClient;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Microsoft.Data.SqlClient;
 
-global using Polly;
-global using Polly.Retry;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Filters;
 
-global using Serilog;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
