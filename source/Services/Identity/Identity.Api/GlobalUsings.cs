32d459d9c0962f1e0759305ed55e655b1673736f
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-02 22:10:07 +0300

    GlobalUsings file updated.

---
+++ source/Services/Identity/Identity.Api/GlobalUsings.cs
@@ -0,0 +1,15 @@
+global using Identity.Api;
+global using Identity.Api.Data;
+global using Identity.Api.Services;
+
+global using Microsoft.IdentityModel.Tokens;
+
+global using Microsoft.EntityFrameworkCore;
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+global using System.Text;
+global using System.Globalization;
+global using System.Security.Claims;
+global using System.IdentityModel.Tokens.Jwt;
