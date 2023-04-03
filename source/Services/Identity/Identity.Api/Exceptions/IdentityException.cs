37649eb9a9055e15f912c7175cee309d6c636c8c
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-03 22:06:21 +0300

    Identity exception added.

---
+++ source/Services/Identity/Identity.Api/Exceptions/IdentityException.cs
@@ -0,0 +1,13 @@
+namespace Identity.Api.Exceptions
+{
+    public class IdentityException : Exception
+    {
+        public IdentityException() { }
+
+        public IdentityException(string message)
+            : base(message) { }
+
+        public IdentityException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
