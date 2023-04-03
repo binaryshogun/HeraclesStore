653909f99aa14bdc187072e4ae51acfe50ab334c
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-03 22:07:35 +0300

    CORS, configuration and health checks added.

--- source/Services/Identity/Identity.Api/IServiceCollectionExtensions.cs
+++ source/Services/Identity/Identity.Api/IServiceCollectionExtensions.cs
@@ -4,7 +4,18 @@ namespace Identity.Api
     {
         public static IServiceCollection AddCustomMVC(this IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options => options.Filters.Add(typeof(HttpGlobalExceptionFilter)));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder
+                        .SetIsOriginAllowed((host) => true)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
 
             return services;
         }
@@ -16,6 +27,44 @@ namespace Identity.Api
                 options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"), sqlOptions =>
                     sqlOptions.EnableRetryOnFailure(15, TimeSpan.FromSeconds(30), null));
             });
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomConfiguration(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Instance = context.HttpContext.Request.Path,
+                        Status = StatusCodes.Status400BadRequest,
+                        Detail = "Please refer to the errors property for additional information"
+                    };
+
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json", "application/problem+xml" }
+                    };
+                };
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            var healthCheckBuilder = services.AddHealthChecks();
+
+            healthCheckBuilder
+                .AddCheck("self", () => HealthCheckResult.Healthy())
+                .AddSqlServer(configuration.GetConnectionString("CatalogDb")!,
+                    name: "CatalogDb-check",
+                    tags: new string[] { "catalogdatabase" });
+
+            // TODO: RabbitMQ health check
 
             return services;
         }
