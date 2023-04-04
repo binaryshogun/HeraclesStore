18f7c21b0773596965495974db2a80cd6cc043b8
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-04 20:41:57 +0300

    Global usings file updated.

--- source/Services/Catalog/Catalog.Api/GlobalUsings.cs
+++ source/Services/Catalog/Catalog.Api/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using Serilog;
 global using AutoMapper;
 
 global using System.IO;
+global using System.Text;
 global using System.ComponentModel.DataAnnotations;
 
 global using Catalog.Api;
@@ -20,8 +21,14 @@ global using Microsoft.Data.SqlClient;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
+global using Microsoft.OpenApi.Models;
+
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Filters;
+
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 global using HealthChecks.UI.Client;
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
