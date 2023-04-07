74a2368388941b140076b7f444c2e259ef07b666
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-07 15:50:19 +0300

    Application services and middleware configured.

--- source/Services/Basket/Basket.Api/Program.cs
+++ source/Services/Basket/Basket.Api/Program.cs
@@ -1,8 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddCustomMVC()
+    .AddEndpointsApiExplorer()
+    .AddCustomConfiguration()
+    .AddCustomAuthentication(builder.Configuration)
+    .AddCustomSwaggerGen()
+    .AddCustomHealthCheck(builder.Configuration)
+    .AddCustomRedis(builder.Configuration);
+
+// Configuring Serilog logger
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("../../../Logs/basket-api-.txt", rollingInterval: RollingInterval.Hour)
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+// Using Serilog as logging provider
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
+builder.Services.AddScoped<IBasketRepository, RedisBasketRepository>();
 
 var app = builder.Build();
 
@@ -12,8 +32,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecks("/liveness", new HealthCheckOptions()
+{
+    Predicate = r => r.Name.Contains("self")
+});
 
 app.Run();
