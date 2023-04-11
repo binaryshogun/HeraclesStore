52d6388d11e683a41a2210e71a3dcbc50128e457
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-11 23:07:28 +0300

    Comments and equality op. implementation added.

--- source/Services/Ordering/Ordering.Domain/SeedWork/ValueObject.cs
+++ source/Services/Ordering/Ordering.Domain/SeedWork/ValueObject.cs
@@ -1,7 +1,16 @@
 namespace Ordering.Domain.SeedWork
 {
+    /// <summary>
+    /// Abstract class representing object containing composite value.
+    /// </summary>
     public abstract class ValueObject
     {
+        /// <summary>
+        /// Compares two <see cref="ValueObject" /> by reference.
+        /// </summary>
+        /// <param name="left">First <see cref="ValueObject" /> value.</param>
+        /// <param name="right">Second <see cref="ValueObject" /> value.</param>
+        /// <returns><see langword="true" /> if values are equal by reference; otherwise - <see langword="false" />.</returns>
         protected static bool EqualOperator(ValueObject left, ValueObject right)
         {
             if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
@@ -11,13 +20,24 @@ namespace Ordering.Domain.SeedWork
             return ReferenceEquals(left, null) || left.Equals(right);
         }
 
+        /// <summary>
+        /// Compares two <see cref="ValueObject" /> by reference.
+        /// </summary>
+        /// <param name="left">First <see cref="ValueObject" /> value.</param>
+        /// <param name="right">Second <see cref="ValueObject" /> value.</param>
+        /// <returns><see langword="true" /> if values are not equal by reference; otherwise - <see langword="false" />.</returns>
         protected static bool NotEqualOperator(ValueObject left, ValueObject right)
         {
             return !(EqualOperator(left, right));
         }
 
-        protected abstract IEnumerable<object> GetEqualityComponents();
+        /// <summary>
+        /// Gets all stored values from value object's properties.
+        /// </summary>
+        /// <returns><see cref="IEnumerable{object?}" /> containing all value object's properties values.</returns>
+        protected abstract IEnumerable<object?> GetEqualityComponents();
 
+        /// <inheritdoc cref="object.Equals(object?)" />
         public override bool Equals(object? obj)
         {
             if (obj is null || obj is not ValueObject)
@@ -41,6 +61,7 @@ namespace Ordering.Domain.SeedWork
             return this.GetEqualityComponents().SequenceEqual(item.GetEqualityComponents());
         }
 
+        /// <inheritdoc cref="object.GetHashCode" />
         public override int GetHashCode()
         {
             return GetEqualityComponents()
@@ -48,9 +69,16 @@ namespace Ordering.Domain.SeedWork
                 .Aggregate((x, y) => x ^ y);
         }
 
+        /// <summary>
+        /// Gets the copy of <see cref="ValueObject" />.
+        /// </summary>
+        /// <returns>Copy of <see cref="ValueObject" /> instance.</returns>
         public ValueObject GetCopy()
         {
             return (this.MemberwiseClone() as ValueObject)!;
         }
+
+        public static bool operator ==(ValueObject left, ValueObject right) => EqualOperator(left, right);
+        public static bool operator !=(ValueObject left, ValueObject right) => NotEqualOperator(left, right);
     }
 }
