1f6517ef882c02e5cc85c76f973e3bcd9844411e
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-12 14:54:41 +0300

    Global usings file updated.

--- source/Services/Ordering/Ordering.Domain/GlobalUsings.cs
+++ source/Services/Ordering/Ordering.Domain/GlobalUsings.cs
@@ -4,4 +4,6 @@ global using System.Reflection;
 
 global using Ordering.Domain.SeedWork;
 global using Ordering.Domain.Exceptions;
+global using Ordering.Domain.Events;
+
 global using Ordering.Domain.Models.OrderAggregate;
