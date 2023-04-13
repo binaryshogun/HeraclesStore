fcfdec886083b2e6b7ac01ca0c55c48790e6c54c
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-13 14:17:28 +0300

    Order repository implementation added.

---
+++ source/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,58 @@
+namespace Ordering.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Implementation of repository for <see cref="Order" /> entity 
+    /// that uses EntityFrameworkCore for operations.
+    /// </summary>
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderingContext context;
+
+        /// <summary>
+        /// Creates new instance of <see cref="OrderRepository" /> with given <paramref name="context" />.
+        /// </summary>
+        /// <param name="context">Current <see cref="OrderingContext" />.</param>
+        /// <exception cref="ArgumentNullException" />
+        public OrderRepository(OrderingContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IUnitOfWork UnitOfWork => context;
+
+        public Order? Get(int orderId)
+        {
+            return context.Orders
+                .Include(o => o.Address)
+                .Include(o => o.OrderItems)
+                .Include(o => o.OrderStatus)
+                .Where(o => o.Id == orderId)
+                .FirstOrDefault();
+        }
+
+        public async Task<Order?> GetAsync(int orderId)
+        {
+            return await context.Orders
+                .Include(o => o.Address)
+                .Include(o => o.OrderItems)
+                .Include(o => o.OrderStatus)
+                .Where(o => o.Id == orderId)
+                .FirstOrDefaultAsync();
+        }
+
+        public Order Add(Order order)
+        {
+            if (order.IsTransient())
+            {
+                return context.Orders.Add(order).Entity;
+            }
+
+            return order;
+        }
+
+        public Order Update(Order order)
+        {
+            return context.Orders.Update(order).Entity;
+        }
+    }
+}
