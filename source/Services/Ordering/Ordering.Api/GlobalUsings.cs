956f7096ab9c42dbb89352010bc124dd793da398
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-17 20:19:22 +0300

    Global usings file updated.

---
+++ source/Services/Ordering/Ordering.Api/GlobalUsings.cs
@@ -0,0 +1,19 @@
+global using Polly;
+global using Polly.Retry;
+
+global using MediatR;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Filters;
+
+global using Microsoft.Data.SqlClient;
+
+global using Microsoft.EntityFrameworkCore;
+
+global using Ordering.Domain.SeedWork;
+global using Ordering.Domain.Exceptions;
+global using Ordering.Domain.Events;
+global using Ordering.Domain.Models.BuyerAggregate;
+global using Ordering.Domain.Models.OrderAggregate;
+
+global using Ordering.Infrastructure;
