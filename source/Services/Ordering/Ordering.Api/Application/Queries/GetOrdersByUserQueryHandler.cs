1d6611ecf9874b490257f8b8240cef90b9ebfb9a
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-18 21:15:08 +0300

    Orders query & query handler added.

---
+++ source/Services/Ordering/Ordering.Api/Application/Queries/GetOrdersByUserQueryHandler.cs
@@ -0,0 +1,36 @@
+namespace Ordering.Api.Application.Queries
+{
+    public class GetOrdersByUserQueryHandler
+        : IRequestHandler<GetOrdersByUserQuery, IEnumerable<OrderSummary>>
+    {
+        private readonly ISqlConnectionFactory connectionFactory;
+
+        public GetOrdersByUserQueryHandler(ISqlConnectionFactory connectionFactory)
+        {
+            this.connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+        }
+
+        public async Task<IEnumerable<OrderSummary>> Handle(GetOrdersByUserQuery request, CancellationToken cancellationToken)
+        {
+            using (var connection = connectionFactory.GetDbConnection())
+            {
+                connection.Open();
+
+                const string ordersByUserSQL =
+                    @"SELECT o.Id, o.OrderDate, os.Name as Status, SUM(oi.Units*oi.UnitPrice) as Total
+                        FROM ordering.orders o
+                        LEFT JOIN ordering.orderitems oi ON o.Id = oi.OrderId
+                        LEFT JOIN ordering.orderstatus os on o.OrderStatusId = os.Id
+                        LEFT JOIN ordering.buyers b on o.BuyerId = b.Id
+                        WHERE b.IdentityId = @userId
+                        GROUP BY o.Id, o.OrderDate, os.Name";
+
+                var orders = await connection.QueryAsync<OrderSummary>(ordersByUserSQL, new { userId = request.UserId });
+
+                connection.Close();
+
+                return orders;
+            }
+        }
+    }
+}
