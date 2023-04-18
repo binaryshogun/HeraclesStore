0aa0021aae7dc42f4050a3669baeeceb12e3e51a
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-18 21:10:12 +0300

    Basket item model added.

---
+++ source/Services/Ordering/Ordering.Api/Application/Models/BasketItem.cs
@@ -0,0 +1,21 @@
+namespace Ordering.Api.Application.Models
+{
+    public class BasketItem
+    {
+        public int Id { get; init; }
+
+        public int ProductId { get; init; }
+
+        [Required]
+        public string? ProductName { get; init; }
+
+        public decimal UnitPrice { get; init; }
+
+        public decimal OldUnitPrice { get; init; }
+
+        [Range(1, 1000)]
+        public int Quantity { get; set; }
+
+        public string? PictureUrl { get; init; }
+    }
+}
