3d405fed0d0c7e802a101665e0f3da79efdd3734
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-18 21:17:52 +0300

    Database initial migration added.

---
+++ source/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20230418161155_Initial.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ordering");
+
+            migrationBuilder.CreateSequence(
+                name: "buyerseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "orderitemseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "orderseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "paymentseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "buyers",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IdentityId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 1000, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_buyers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cardtypes",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cardtypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderstatus",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderstatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "requests",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_requests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "paymentmethods",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CardTypeId = table.Column<int>(type: "int", nullable: false),
+                    BuyerId = table.Column<int>(type: "int", nullable: false),
+                    Alias = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CardHolderName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Expiration = table.Column<DateTime>(type: "datetime2", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paymentmethods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_paymentmethods_buyers_BuyerId",
+                        column: x => x.BuyerId,
+                        principalSchema: "ordering",
+                        principalTable: "buyers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_paymentmethods_cardtypes_CardTypeId",
+                        column: x => x.CardTypeId,
+                        principalSchema: "ordering",
+                        principalTable: "cardtypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    BuyerId = table.Column<int>(type: "int", nullable: true),
+                    PaymentMethodId = table.Column<int>(type: "int", nullable: true),
+                    Address_Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrderStatusId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orders_buyers_BuyerId",
+                        column: x => x.BuyerId,
+                        principalSchema: "ordering",
+                        principalTable: "buyers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_orders_orderstatus_OrderStatusId",
+                        column: x => x.OrderStatusId,
+                        principalSchema: "ordering",
+                        principalTable: "orderstatus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orders_paymentmethods_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalSchema: "ordering",
+                        principalTable: "paymentmethods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderitems",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discount = table.Column<decimal>(type: "decimal(8,2)", precision: 8, scale: 2, nullable: false, defaultValue: 0m),
+                    PictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Units = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(8,2)", precision: 8, scale: 2, nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderitems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orderitems_orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_buyers_IdentityId",
+                schema: "ordering",
+                table: "buyers",
+                column: "IdentityId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderitems_OrderId",
+                schema: "ordering",
+                table: "orderitems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_BuyerId",
+                schema: "ordering",
+                table: "orders",
+                column: "BuyerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_OrderStatusId",
+                schema: "ordering",
+                table: "orders",
+                column: "OrderStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_PaymentMethodId",
+                schema: "ordering",
+                table: "orders",
+                column: "PaymentMethodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_paymentmethods_BuyerId",
+                schema: "ordering",
+                table: "paymentmethods",
+                column: "BuyerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_paymentmethods_CardTypeId",
+                schema: "ordering",
+                table: "paymentmethods",
+                column: "CardTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orderitems",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "requests",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "orders",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "orderstatus",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "paymentmethods",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "buyers",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "cardtypes",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "buyerseq",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "orderitemseq",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "orderseq",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "paymentseq",
+                schema: "ordering");
+        }
+    }
+}
