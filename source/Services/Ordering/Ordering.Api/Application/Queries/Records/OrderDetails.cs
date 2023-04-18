30e1c52667c093e8772a7ea751f5cc58a3315088
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-18 21:13:40 +0300

    Record types for queries added.

---
+++ source/Services/Ordering/Ordering.Api/Application/Queries/Records/OrderDetails.cs
@@ -0,0 +1,18 @@
+namespace Ordering.Api.Application.Queries.Records
+{
+    public record OrderDetails
+    {
+        public int Id { get; init; }
+        public DateTime Date { get; init; }
+        public string? Status { get; init; }
+        public string? Description { get; init; }
+        public string? Street { get; init; }
+        public string? City { get; init; }
+        public string? State { get; init; }
+        public string? Country { get; init; }
+        public string? ZipCode { get; init; }
+        public decimal Total { get; init; }
+
+        public List<OrderItemSummary>? OrderItems { get; set; }
+    }
+}
