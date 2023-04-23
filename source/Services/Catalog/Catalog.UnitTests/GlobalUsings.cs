e7d3c413ceb876102e64c841e166e9a45b3602b0
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-23 20:32:33 +0300

    Global usings file updated.

--- source/Services/Catalog/Catalog.UnitTests/GlobalUsings.cs
+++ source/Services/Catalog/Catalog.UnitTests/GlobalUsings.cs
@@ -1,6 +1,14 @@
 global using Moq;
 global using Xunit;
+global using AutoMapper;
 
 global using Catalog.Api.Models;
-global using Catalog.Api.Data;
 global using Catalog.Api.Exceptions;
+global using Catalog.Api.Controllers;
+global using Catalog.Api.Profiles;
+global using Catalog.Api.Data;
+global using Catalog.Api.Data.Dtos;
+
+global using Catalog.UnitTests.Mocks;
+
+global using Microsoft.AspNetCore.Mvc;
