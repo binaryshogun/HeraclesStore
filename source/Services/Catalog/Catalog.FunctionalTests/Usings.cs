1a3d50c0d3227727ea395368d07698652cf5ad01
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-04-25 23:31:44 +0300

    feat: add functional test for catalog; set up tests infrastructure; add test scenarios
    Create xUnit test project for catalog functional tests.
    
    Create test infrastructure for catalog functional tests:
      add configuration files and test database context.
    
    Write test scenarions for catalog API endpoints.
    
    closes #20

---
+++ source/Services/Catalog/Catalog.FunctionalTests/Usings.cs
@@ -0,0 +1,28 @@
+global using Xunit;
+
+global using Catalog.Api.Data;
+global using Catalog.Api.Data.Dtos;
+
+global using Catalog.FunctionalTests.Services;
+global using Catalog.FunctionalTests.Data;
+
+global using System.Text;
+
+global using System.Reflection;
+
+global using System.Net;
+global using System.Net.Http.Json;
+global using System.Net.Http.Headers;
+
+global using System.IdentityModel.Tokens.Jwt;
+
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.AspNetCore.Hosting;
+
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using Microsoft.EntityFrameworkCore;
+
+global using Microsoft.IdentityModel.Tokens;
